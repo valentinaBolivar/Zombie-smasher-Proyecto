7c83151f6ca7d37850f4fa72dce503b85b6db6e8
unknown <SENA@MEDAPRCCSFSD337.FormacionSena.RED>
2022-06-15 15:10:26 -0500

    cambio 2

---
+++ Nubes/Assets/interaccionesPlayer/InterP.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
